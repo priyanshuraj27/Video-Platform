85ef663eb11e816aec2aa4b8b008ef51df2abb2b
priyanshuraj27 <priyanshuraj1250@gmail.com>
2025-03-05 18:44:21 +0530

    All configured

--- src/app.js
+++ src/app.js
@@ -2,13 +2,23 @@ import express, { urlencoded } from 'express';
 import cookieParser from 'cookie-parser';
 import cors from 'cors';
 const app = express();
-
+// console.log("app.js");
 app.use(cors({
     origin : process.env.CORS_ORIGIN,
     credentials : true
 }))
+
  app.use(express.json({limit : "16kb"}));
  app.use(urlencoded({extended : true}));
  app.use(express.static('public'));
  app.use(cookieParser());
+ 
+// Routes import 
+
+import userRouter from './routes/user.routes.js';
+
+// routes declaration
+
+app.use("/api/v1/users",userRouter);
+
 export {app};
--- src/index.js
+++ src/index.js
@@ -1,15 +1,18 @@
 import dotenv from 'dotenv';
+import express from 'express';
 import connectDB from './db/index.js';
+import { app } from './app.js';
 dotenv.config({
     path: "./env"
 });
-connectDB()
 
-.then(() =>{
-    app.listen(process.env.PORT || 8000,()=>{
-        console.log(`Server is running on port ${process.env.PORT || 8000}`);
+
+connectDB()
+    .then(() => {
+        app.listen(process.env.PORT || 8000, () => {
+            console.log(`Server is running on port ${process.env.PORT || 8000}`);
+        });
     })
-})
-.catch((error) => {
-    console.log("MONGO DB connection failed ",error);
-})
+    .catch((error) => {
+        console.log("MONGO DB connection failed ", error);
+    });
