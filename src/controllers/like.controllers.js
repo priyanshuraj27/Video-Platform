eca89516c12411dc943f9173ce40b7dee5e252c3
priyanshuraj27 <priyanshuraj1250@gmail.com>
2025-03-19 18:18:03 +0530

    Implement like functionality for videos, comments, and tweets; add validation and retrieval of liked videos

--- src/controllers/like.controllers.js
+++ src/controllers/like.controllers.js
@@ -7,23 +7,77 @@ import {asyncHandler} from "../utils/asyncHandler.js"
 const toggleVideoLike = asyncHandler(async (req, res) => {
     const {videoId} = req.params
     //TODO: toggle like on video
-    
+    const {user} = req.user
+    const like = await Like.findOne({video : videoId, likedBy : user._id})
+    if(!isValidObjectId(videoId)){
+        throw new ApiError(400, "Invalid video id")
+    }
+    if(like){
+        await like.remove();
+        res.status(200).json(new ApiResponse(200, "Like removed successfully", like))
+    }
+    else{
+        const newLike = new Like({
+            video : videoId,
+            likedBy : user._id
+        })
+        await newLike.save()
+        res.status(201).json(new ApiResponse(201, "Liked successfully", newLike))
+    }
 })
 
 const toggleCommentLike = asyncHandler(async (req, res) => {
     const {commentId} = req.params
     //TODO: toggle like on comment
+    const {user} = req.user
+    const like = await Like.findOne({video : commentId, likedBy : user._id})
+    if(!isValidObjectId(commentId)){
+        throw new ApiError(400, "Invalid comment id")
+    }
+    if(like){
+        await like.remove();
+        res.status(200).json(new ApiResponse(200, "Like removed successfully", like))
+    }
+    else{
+        const newLike = new Like({
+            video : commentId,
+            likedBy : user._id
+        })
+        await newLike.save()
+        res.status(201).json(new ApiResponse(201, "Liked successfully", newLike))
+    }
 
 })
 
 const toggleTweetLike = asyncHandler(async (req, res) => {
     const {tweetId} = req.params
     //TODO: toggle like on tweet
+    const {user} = req.user
+    const like = await Like.findOne({video : tweetId, likedBy : user._id})
+    if(!isValidObjectId(tweetId)){
+        throw new ApiError(400, "Invalid Tweet id")
+    }
+    if(like){
+        await like.remove();
+        res.status(200).json(new ApiResponse(200, "Like removed successfully", like))
+    }
+    else{
+        const newLike = new Like({
+            video : tweetId,
+            likedBy : user._id
+        })
+        await newLike.save()
+        res.status(201).json(new ApiResponse(201, "Liked successfully", newLike))
+    }
+
 }
 )
 
 const getLikedVideos = asyncHandler(async (req, res) => {
     //TODO: get all liked videos
+    const {user} = req.user
+    const likedVideos = await Like.find({likedBy : user._id}).populate("video")
+    res.status(200).json(new ApiResponse(200, "Liked videos fetched successfully", likedVideos))
 })
 
 export {
