c68c82216efcaa329354db7e93243e8a9a8cf323
priyanshuraj27 <priyanshuraj1250@gmail.com>
2025-03-19 18:28:06 +0530

    Implement subscription toggle functionality and enhance subscriber/channel retrieval

--- src/controllers/subscription.controllers.js
+++ src/controllers/subscription.controllers.js
@@ -9,16 +9,34 @@ import {asyncHandler} from "../utils/asyncHandler.js"
 const toggleSubscription = asyncHandler(async (req, res) => {
     const {channelId} = req.params
     // TODO: toggle subscription
+    const {subscriberId} = req.user
+    const subscription = await Subscription.findOne({channelId, subscriberId})
+    if(subscription){
+        await Subscription.findByIdAndDelete(subscription._id)
+        res.status(200).json(new ApiResponse(200,{},"Unsubscribed successfully"))
+    }
+    else{
+        const subscription = new Subscription({
+            channelId,
+            subscriberId
+        })
+        .save();
+        res.status(200).json(new ApiResponse(200,{subscription},"Subscribed successfully"))
+    }
 })
 
 // controller to return subscriber list of a channel
 const getUserChannelSubscribers = asyncHandler(async (req, res) => {
     const {channelId} = req.params
+    const subscribers = await Subscription.find({channelId}).populate("subscriberId")
+    res.status(200).json(new ApiResponse(200,{subscribers},"Subscribers fetched successfully"))
 })
 
 // controller to return channel list to which user has subscribed
 const getSubscribedChannels = asyncHandler(async (req, res) => {
     const { subscriberId } = req.params
+    const channels = await Subscription.find({subscriberId}).populate("channelId")
+    res.status(200).json(new ApiResponse(200,{channels},"Subscribed channels fetched successfully"))
 })
 
 export {
