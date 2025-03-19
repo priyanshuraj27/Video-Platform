e50423697062af16e34d8600dbefce9036fa25f5
priyanshuraj27 <priyanshuraj1250@gmail.com>
2025-03-19 18:23:09 +0530

    Add channel statistics and videos retrieval: implement total views, subscribers, likes, and fetch user's videos

--- src/controllers/dashboard.controllers.js
+++ src/controllers/dashboard.controllers.js
@@ -8,11 +8,35 @@ import {asyncHandler} from "../utils/asyncHandler.js"
 
 const getChannelStats = asyncHandler(async (req, res) => {
     // TODO: Get the channel stats like total video views, total subscribers, total videos, total likes etc.
-    
+    const userId = req.user._id
+    const totalVideos = await Video.countDocuments({user: userId})
+    const totalViews = await Video.aggregate([
+        {
+            $match: {user: mongoose.Types.ObjectId(userId)}
+        },
+        {
+            $group: {
+                _id: null,
+                totalViews: {$sum: "$views"}
+            }
+        }
+    ])
+    const totalSubscribers = await Subscription.countDocuments({channel: userId})
+    const totalLikes = await Like.countDocuments({user: userId})
+    const response = {
+        totalVideos,
+        totalViews: totalViews.length ? totalViews[0].totalViews : 0,
+        totalSubscribers,
+        totalLikes
+    }
+    res.status(201).json(new ApiResponse(201,response,"Channel stats fetched successfully"))
 })
 
 const getChannelVideos = asyncHandler(async (req, res) => {
     // TODO: Get all the videos uploaded by the channel
+    const userId = req.user._id
+    const videos = await Video.find({user: userId})
+    res.status(201).json(new ApiResponse(201,videos,"Channel videos fetched successfully"))
 })
 
 export {
